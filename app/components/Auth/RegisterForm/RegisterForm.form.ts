4e127b1ce971bd0426eaf38ef2b12ac6b3d326a3
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-03 14:57:14 -0500

    added register form with formik and yup

---
+++ app/components/Auth/RegisterForm/RegisterForm.form.ts
@@ -0,0 +1,27 @@
+import * as Yup from 'yup'
+import type { FormikConfig } from 'formik';
+
+interface FormValues {
+    email: string;
+    username: string;
+    name: string;
+    password: string;
+}
+
+export function initialValues() : FormValues {
+    return {
+        email: "",
+        username: "",
+        name: "",
+        password: "",
+    };
+}
+
+export function validationSchema() {
+    return Yup.object({
+        email: Yup.string().email().required(),
+        username: Yup.string().required(),
+        name: Yup.string().required(),
+        password: Yup.string().required(),
+    });
+}
--- app/components/Auth/RegisterForm/RegisterForm.tsx
+++ app/components/Auth/RegisterForm/RegisterForm.tsx
@@ -1,20 +1,60 @@
 "use client"
 
 import { Form } from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import { initialValues, validationSchema } from './RegisterForm.form'
 
 export function RegisterForm() {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+        onSubmit: (formValue) => {
+            console.log("FORMULARIO ENVIADO");
+            console.log(formValue);
+        }
+    })
     return (
-        <Form>
+        <Form onSubmit={formik.handleSubmit}>
             <Form.Group widths="equal">
-                <Form.Input name="email" type="text" placeholder="Correo electrónico" />
-                <Form.Input name="username" type="text" placeholder="Nombre de usuario" />
+                <Form.Input
+                    name="email"
+                    type="text"
+                    placeholder="Correo electrónico"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.errors.email}
+                />
+                <Form.Input
+                    name="username"
+                    type="text"
+                    placeholder="Nombre de usuario"
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    error={formik.errors.username}
+                />
             </Form.Group>
             <Form.Group widths="equal">
-                <Form.Input name="name" type="text" placeholder="Nombre y apellidos" />
-                <Form.Input name="password" type="password" placeholder="Contraseña" />
+                <Form.Input
+                    name="name"
+                    type="text"
+                    placeholder="Nombre y apellidos"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    error={formik.errors.name}
+                />
+                <Form.Input
+                    name="password"
+                    type="password"
+                    placeholder="Contraseña"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.errors.password}
+                />
             </Form.Group>
 
-            <Form.Button type="submit" fluid>
+            <Form.Button type="submit" fluid loading={formik.isSubmitting}>
                 Registrarse
             </Form.Button>
         </Form>
