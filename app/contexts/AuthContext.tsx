b6904e0cee574a0be65d93bb20903c0df1b7cb53
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-07 11:22:18 -0500

    added updateUser function

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -1,16 +1,44 @@
 "use client"
 import { createContext, useEffect, useState } from "react";
 import { Token, User } from "@/api";
+import { StrictSegmentInlineProps } from "semantic-ui-react";
+
+interface IUser {
+    id: number;
+    username: string;
+    email: string;
+    provider: string;
+    confirmed: boolean;
+    blocked: boolean;
+    firstname: null | string;
+    lastname: null | string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export type AuthContextType = {
+    accessToken: null | string;
+    user: null | IUser;
+    login: (token: string) => void;
+    logout: () => void;
+    updateUser: (key: string, value: any) => void;
+}
 
 const tokenCtrl = new Token();
 const userCtrl = new User();
 
-export const AuthContext = createContext();
+export const AuthContext = createContext<AuthContextType>({
+    accessToken: null,
+    user: null,
+    login: () => {},
+    logout: () => {},
+    updateUser: () => {},
+});
 
 export function AuthProvider({ children }: {
     children: React.ReactNode;
 }) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState<null | IUser>(null);
     const [token, setToken] = useState<null | string>(null);
     const [loading, setLoading] = useState<boolean>(true);
 
@@ -54,12 +82,19 @@ export function AuthProvider({ children }: {
         setUser(null);
     }
 
+    const updateUser = (key: string, value: any) => {
+        setUser({
+            ...user,
+            [key]: value,
+        });
+    }
+
     const data = {
         accessToken: token,
         user,
         login,
         logout,
-        updateUser: null,
+        updateUser,
     };
 
     if (loading) return null;
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,7 @@
-"use client"
-
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "semantic-ui-react";
-import { useAuth } from "@/hooks";
-
 export default function Home() {
-  const { user, logout } = useAuth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Games Shop</h2>
-      <Button primary>Ir al Login</Button>
-
-      {user ? (
-        <div>
-          <p>
-            Hola, {user.firstname} {user.lastname}
-          </p>
-          <Button onClick={logout}>Cerrar Sesión</Button>
-        </div>
-      ) : (
-        <div>
-          <Link href="/auth/sign-in">Iniciar Sesion</Link>
-        </div>
-      )}
     </main>
   );
 }
