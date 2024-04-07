a64a6cfcbee429149e257a66c21e1b3c1852fd6c
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-07 14:18:19 -0500

    added inputSearch to Menu

--- app/components/Layout/Menu/Menu.tsx
+++ app/components/Layout/Menu/Menu.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import { Image, Icon, Input } from "semantic-ui-react"
 import Link from "next/link"
 import { map } from "lodash"
+import classNames from "classnames"
 import { Platform } from "@/api"
 import { ENV } from "@/utils"
 import styles from "./Menu.module.scss"
@@ -11,6 +12,9 @@ const platformCtrl = new Platform();
 
 export function Menu({ isOpenSearch }: Readonly<{ isOpenSearch: boolean }>) {
     const [platforms, setPlatforms] = useState(null)
+    const [showSearch, setShowSearch] = useState<boolean>(false)
+
+    const openCloseSearch = () => setShowSearch(prevState => !prevState);
 
     useEffect(() => {
       (async () => {
@@ -31,6 +35,26 @@ export function Menu({ isOpenSearch }: Readonly<{ isOpenSearch: boolean }>) {
                     {platform.attributes.title}
                 </Link>
             ))}
+
+            <button className={styles.search} onClick={openCloseSearch}>
+                <Icon name="search" />
+            </button>
+
+            <div className={classNames(styles.inputContainer, {
+                [styles.active]: showSearch
+            })}>
+                <Input
+                    id="search-games"
+                    placeholder="Buscador"
+                    className={styles.input}
+                    focus={true}
+                />
+                <Icon
+                    name="close"
+                    className={styles.closeInput}
+                    onClick={openCloseSearch}
+                />
+            </div>
         </div>
     )
 }
