7f54115084b423a441f1217030796886297bf065
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-07 19:27:46 -0500

    added Account Info

---
+++ app/account/page.tsx
@@ -0,0 +1,12 @@
+import BasicLayout from "@/BasicLayout"
+import { Info } from "@/components/Account"
+
+export default function AccountPage() {
+    return (
+        <>
+            <BasicLayout isContainer relative>
+                <Info />
+            </BasicLayout>
+        </>
+    )
+}
