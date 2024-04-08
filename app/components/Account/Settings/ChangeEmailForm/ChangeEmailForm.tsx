068473ba51d0b3348f0804de7922b752562a6372
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-07 23:50:44 -0500

    added function to update email

--- app/components/Account/Settings/ChangeEmailForm/ChangeEmailForm.tsx
+++ app/components/Account/Settings/ChangeEmailForm/ChangeEmailForm.tsx
@@ -1,16 +1,24 @@
 import { Form } from "semantic-ui-react"
 import { useFormik } from "formik"
+import { User } from "@/api"
+import { useAuth } from "@/hooks"
 import { initialValues, validationSchema } from "./ChangeEmailForm.form"
 import styles from "./ChangeEmailForm.module.scss"
 
+const userCtrl = new User();
+
 export function ChangeEmailForm() {
+    const { user, updateUser } = useAuth();
+
     const formik = useFormik({
         initialValues: initialValues(),
         validationSchema: validationSchema(),
         validateOnChange: false,
         onSubmit: async (formValue) => {
             try {
-                console.log(formValue);
+                await userCtrl.updateMe(user.id, { email: formValue.email });
+                updateUser("email", formValue.email)
+                formik.handleReset();
             } catch (error) {
                 console.error(error);
             }
